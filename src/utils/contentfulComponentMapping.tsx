f036ecc1a8b818eb6eece504b17d167279e8577f
Elif Dogali <dogali.elif@gmail.com>
2023-01-27 00:51:15 +0300

    Add paralax

--- src/utils/contentfulComponentMapping.tsx
+++ src/utils/contentfulComponentMapping.tsx
@@ -1,5 +1,6 @@
 import BlogTiles from '@/components/cms/blogTile/BlogTiles'
 import Carousel from '@/components/cms/carousel/Carousel'
+import ParallaxImage from '@/components/cms/ParallaxImage'
 
 interface mapping {
   [key: string]: (props: any) => JSX.Element
@@ -8,6 +9,7 @@ interface mapping {
 const componentMapping: mapping = {
   Carousel: Carousel,
   BlogTiles: BlogTiles,
+  ParallaxImage: ParallaxImage,
 }
 
 export default componentMapping
